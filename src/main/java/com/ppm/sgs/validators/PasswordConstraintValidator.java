8e568d30c3ef832131bc875ce118dcc75a4c6ed1
Paing Pyae Man <paingpyaeman04@gmail.com>
2023-06-13 11:15:34 +0630

    Add custom password validator

---
+++ src/main/java/com/ppm/sgs/validators/PasswordConstraintValidator.java
@@ -0,0 +1,42 @@
+package com.ppm.sgs.validators;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.passay.CharacterRule;
+import org.passay.EnglishCharacterData;
+import org.passay.LengthRule;
+import org.passay.PasswordData;
+import org.passay.PasswordValidator;
+import org.passay.RuleResult;
+import org.passay.WhitespaceRule;
+
+public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, String> {
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        PasswordValidator validator = new PasswordValidator(Arrays.asList(
+                new LengthRule(8, 24),
+                new CharacterRule(EnglishCharacterData.UpperCase, 1),
+                new CharacterRule(EnglishCharacterData.LowerCase, 1),
+                new CharacterRule(EnglishCharacterData.Digit, 1),
+                new CharacterRule(EnglishCharacterData.Special, 1),
+                new WhitespaceRule()));
+
+        RuleResult result = validator.validate(new PasswordData(value));
+        if (result.isValid()) {
+            return true;
+        }
+
+        List<String> messages = validator.getMessages(result);
+        String messageTemplate = String.join(",", messages);
+        context.buildConstraintViolationWithTemplate(messageTemplate)
+                .addConstraintViolation()
+                .disableDefaultConstraintViolation();
+        return false;
+    }
+
+}
