65e5cf6744b4d5311e482db5a0615b6db4d914c2
Paing Pyae Man <paingpyaeman04@gmail.com>
2023-06-13 11:15:54 +0630

    Change from ModelAndView to Model

--- src/main/java/com/ppm/sgs/controllers/AuthController.java
+++ src/main/java/com/ppm/sgs/controllers/AuthController.java
@@ -3,10 +3,9 @@ package com.ppm.sgs.controllers;
 import java.security.Principal;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.ppm.sgs.dtos.LoginDto;
 
@@ -15,20 +14,16 @@ import com.ppm.sgs.dtos.LoginDto;
 public class AuthController {
     
     @GetMapping("/")
-    public ModelAndView showMainMenu(ModelAndView mav, Principal p) {
+    public String showMainMenu(Model model, Principal p) {
         String name = p.getName();
-        mav.addObject("name", name);
-        mav.setViewName("main-menu");
-        return mav;
+        model.addAttribute("name", name);
+        return "main-menu";
     }
     
     @GetMapping("/signin")
-    public String loginForm() {
+    public String loginForm(Model model) {
+        model.addAttribute("loginDto", new LoginDto());
         return "login";
     }
-    
-    @ModelAttribute("loginDto")
-    public LoginDto loginDto() {
-        return new LoginDto();
-    }
+
 }
