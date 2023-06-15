ffe24f68908a984f5a0bcbcdcf8e35fb1b1cd8f6
Paing Pyae Man <paingpyaeman04@gmail.com>
2023-06-15 06:30:31 +0630

    Add new query methods

--- src/main/java/com/ppm/sgs/repositories/UserRepository.java
+++ src/main/java/com/ppm/sgs/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package com.ppm.sgs.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,16 @@ import org.springframework.stereotype.Repository;
 import com.ppm.sgs.models.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByUsername(String username);
+
+    List<User> findByEnabled(Boolean isEnabled);
+
+    List<User> findByIdOrName(String id, String name);
+
+    Optional<User> findFirstByOrderByIdDesc();
+
+    boolean existsByUsername(String username);
+    
+    boolean existsByEmail(String email);
 }
