c27db8827928d6865260bf210f38876afcda81a4
Paing Pyae Man <paingpyaeman04@gmail.com>
2023-06-22 10:58:48 +0630

    Add resource not found and exists exceptions

---
+++ src/main/java/com/ppm/sgs/exceptions/ResourceAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.ppm.sgs.exceptions;
+
+public class ResourceAlreadyExistsException extends RuntimeException {
+    public ResourceAlreadyExistsException(String s) {
+        super(s);
+    }
+}
