f7e97b62acf7763626775bb0b22a52ff90c5b583
Paing Pyae Man <paingpyaeman04@gmail.com>
2023-06-26 11:07:03 +0630

    Add some methods for student crud

--- src/main/java/com/ppm/sgs/repositories/StudentRepository.java
+++ src/main/java/com/ppm/sgs/repositories/StudentRepository.java
@@ -1,9 +1,20 @@
 package com.ppm.sgs.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ppm.sgs.models.Student;
 
 public interface StudentRepository extends JpaRepository<Student, String> {
-    
+    boolean existsByPhone(String phone);
+
+    Optional<Student> findByPhone(String phone);
+
+    Optional<Student> findFirstByOrderByIdDesc();
+
+    @Query("SELECT s FROM Student s WHERE (:id is null or s.id LIKE :id) AND (:name is null or s.name LIKE :name)")
+    List<Student> findByIdAndName(String id, String name);
 }
