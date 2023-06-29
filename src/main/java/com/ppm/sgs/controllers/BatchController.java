3b454df3c8f53cbed1ae02d15b5794ca6a7d5a7d
Paing Pyae Man <paingpyaeman04@gmail.com>
2023-06-29 21:06:22 +0630

    Add batch update & reporting

--- src/main/java/com/ppm/sgs/controllers/BatchController.java
+++ src/main/java/com/ppm/sgs/controllers/BatchController.java
@@ -1,7 +1,12 @@
 package com.ppm.sgs.controllers;
 
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +20,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ppm.sgs.dtos.StudentReportDto;
 import com.ppm.sgs.models.Batch;
 import com.ppm.sgs.models.BatchDto;
 import com.ppm.sgs.models.Course;
+import com.ppm.sgs.models.Student;
 import com.ppm.sgs.models.User;
 import com.ppm.sgs.services.BatchService;
 import com.ppm.sgs.services.CourseService;
 import com.ppm.sgs.services.UserService;
+import com.ppm.sgs.utils.ReportUtils;
 
 @Controller
 @RequestMapping("/batches")
@@ -47,28 +55,65 @@ public class BatchController {
 
     @PostMapping("add")
     public String addNewBatch(ModelMap map, @Valid @ModelAttribute("batch") BatchDto batchDto, BindingResult result) {
-        if(result.hasErrors()) {
+        if (result.hasErrors()) {
             map.addAttribute("courseId", batchDto.getCourseId());
             return "add-batch";
         }
-        
+
         Batch batch = batchService.convertDtoToEntity(batchDto);
         batchService.save(batch);
 
         return "redirect:/courses/" + batchDto.getCourseId() + "/batches";
     }
 
-    @GetMapping("{id}")
+    @GetMapping("{id}/students")
     public String batchDetail(@PathVariable("id") Integer id, ModelMap map) {
         Batch batch = batchService.getById(id);
         map.addAttribute("batch", batch);
         return "batch-detail";
     }
 
+    @GetMapping("{id}/report/batch-students")
+    public void report(@PathVariable("id") Integer id, @RequestParam("type") String type,
+            HttpServletResponse response) {
+        Batch batch = batchService.getById(id);
+
+        List<StudentReportDto> dtos = convertBatchToStudentReportDtos(batch);
+        Map<String, Object> parameters = buildStudentListParameters(batch);
+
+        ReportUtils.reportStudentListForBatch(response, parameters, dtos, type);
+    }
+
+    @GetMapping("{id}")
+    public String displayUpdateForm(ModelMap map, @PathVariable("id") Integer id) {
+        Batch batch = batchService.getById(id);
+        String courseId = batch.getCourse().getId();
+        BatchDto batchDto = new BatchDto(id, courseId, batch.getStartDate(),
+                batch.getEndDate(), batch.getLecturer().getId());
+        map.addAttribute("batch", batchDto);
+        map.addAttribute("course", batch.getCourse());
+        return "batch-update";
+    }
+
+    @PostMapping("update")
+    public String updateBatch(ModelMap map, @Valid @ModelAttribute("batch") BatchDto batchDto, BindingResult result) {
+        if (result.hasErrors()) {
+            Course course = courseService.getById(batchDto.getCourseId());
+            map.addAttribute("course", course);
+            return "batch-update";
+        }
+
+        Batch batch = batchService.convertDtoToEntity(batchDto);
+        batch.setId(batchDto.getId());
+        batchService.update(batch);
+
+        return "redirect:/courses/" + batchDto.getCourseId() + "/batches";
+    }
+
     @GetMapping("/remove")
     public String removeStudent(@RequestParam("id") Integer batchId, @RequestParam("student-id") String studentId) {
         batchService.removeStudent(batchId, studentId);
-        return "redirect:" + batchId;
+        return "redirect:" + batchId + "/students";
     }
 
     @PostMapping("/delete")
@@ -77,7 +122,7 @@ public class BatchController {
         return "redirect:/courses/" + courseId + "/batches";
     }
 
-    /* Model Attributes */ 
+    /* Model Attributes */
 
     @ModelAttribute("courses")
     public List<Course> courses() {
@@ -88,6 +133,28 @@ public class BatchController {
     @ModelAttribute("lecturers")
     public List<User> lecturers() {
         return userService.getLecturers();
+    }
+
+    /* Helper methods */
+    private List<StudentReportDto> convertBatchToStudentReportDtos(Batch batch) {
+        List<Student> students = batch.getStudents();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+
+        return students.stream()
+                .map(std -> new StudentReportDto(std.getId(), std.getName(),
+                        std.getGender().equals('M') ? "Male" : "Female", std.getPhone(), sdf.format(std.getDob())))
+                .collect(Collectors.toList());
+    }
+
+    private Map<String, Object> buildStudentListParameters(Batch batch) {
+        String courseName = batch.getCourse().getName();
+        Short batchNumber = batch.getNumber();
+        String lecturerName = batch.getLecturer().getName();
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("courseDetail", courseName + " (Batch - " + batchNumber + ")");
+        parameters.put("lecturerDetail", "Lecturer - " + lecturerName);
+        return parameters;
     }
 
 }
