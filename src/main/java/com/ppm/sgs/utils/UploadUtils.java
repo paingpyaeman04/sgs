f54bd6600523de9f87f2c65371772617f6e80c62
Paing Pyae Man <paingpyaeman04@gmail.com>
2023-07-04 09:14:44 +0630

    Read json value to get url

--- src/main/java/com/ppm/sgs/utils/UploadUtils.java
+++ src/main/java/com/ppm/sgs/utils/UploadUtils.java
@@ -4,34 +4,40 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class UploadUtils {
-	
-	public static String dummyUpload(byte[] data, String contentType) throws IOException{
-		return "this_is_url";
-	}
-	
-	public static String upload(byte[] data, String contentType) throws IOException {
-		URL url = new URL("https://api.upload.io/v2/accounts/kW15bPc/uploads/binary");
+
+    public static String dummyUpload(byte[] data, String contentType) throws IOException {
+        return "this_is_url";
+    }
+
+    public static String upload(byte[] data, String contentType) throws IOException {
+        URL url = new URL("https://api.upload.io/v2/accounts/FW25bU3/uploads/binary");
         HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
         httpConn.setRequestMethod("POST");
 
         httpConn.setRequestProperty("Authorization", "Bearer " + System.getenv("UPLOAD_TOKEN"));
         httpConn.setRequestProperty("Content-Type", contentType);
         httpConn.setDoOutput(true);
-        
-        try(DataOutputStream wr = new DataOutputStream(httpConn.getOutputStream())) {
+
+        try (DataOutputStream wr = new DataOutputStream(httpConn.getOutputStream())) {
             wr.write(data);
         }
 
         String response = "";
-        if(httpConn.getResponseCode() / 100 == 2) {
+        if (httpConn.getResponseCode() / 100 == 2) {
             Scanner s = new Scanner(httpConn.getInputStream());
             response = s.hasNext() ? s.next() : "";
         }
-        
-        return response;
-	}
-}
 
+        Map<String, String> resMap = new ObjectMapper().readValue(response, HashMap.class);
+
+        return resMap.get("fileUrl");
+    }
+
+}
